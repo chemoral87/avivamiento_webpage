591d788a2d95b98c6f62f086d4e789f05724b152
Sergio Morales <chemoral87@hotmail.com>
2026-06-23 15:59:38 -0600

    add classifications

--- app/constants/classifications.js
+++ app/constants/classifications.js
@@ -1,7 +1,8 @@
 export const classifications = [
-  { value: 'general',     label: 'General',     hex: '#9e9e9e' },
-  { value: 'jv3s',        label: 'JV3S',        hex: '#f97316' },
-  { value: 'jv3s-teen',   label: 'JV3S Teen',   hex: '#2196f3' },
+  { value: 'general', label: 'General', hex: '#9e9e9e' },
+  { value: 'kids', label: 'Kids', hex: '#4CAF50' },
+  { value: 'jv3s', label: 'JV3S', hex: '#f97316' },
+  { value: 'jv3s-teen', label: 'JV3S Teen', hex: '#2196f3' },
   { value: 'jv3s-legado', label: 'JV3S Legado', hex: '#f44336' },
 ]
 
